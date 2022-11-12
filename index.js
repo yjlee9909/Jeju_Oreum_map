24865f7e70146dd9f278b4e2dab15efd4f39ebcd
yjlee9909 <yjlee9909@naver.com>
2022-11-13 00:46:04 +0900

    마커 추가

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const data = marker;
-function initMap() {
+let dataList = [];
+async function initMap() {
+    
+    // 데이터 불러오기
+    let perPageNum = 10;
+    for (let pageNum = 1; pageNum <= perPageNum ; pageNum++) {
+
+        const response = await fetch(
+            `https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=${pageNum}&perPage=10&serviceKey=uD0LdhujYQQfWxU1YCR6Qyn%2BIesISjKYwPNXK90c0GwfHJxYqrR13xA7T4vM84mXNrPuWMrxKr%2BqEuFXY9v3nQ%3D%3D`)
+        .then(data => data.json())
+        .then (data => {return data});
+        dataList.push(...response.data);
+    }
+    console.log(dataList);
+
+    // dataList에서 위도 경도 뽑아서 출력하기
+
+    // 마커 찍기
+
     const jeju = { lat: 33.3616658, lng: 126.5204118 };
     const map = new google.maps.Map(
         document.getElementById("map"),
@@ -8,12 +25,18 @@ function initMap() {
             center: jeju,
         }
     );
+
+    for (let idx = 0; idx < dataList.length; idx++) {
+        let marker = new google.maps.Marker({
+            position: {lat: parseFloat(dataList[idx].위도), lng: parseFloat(dataList[idx].경도)},
+            map: map,
+        });
+    }
+    
+
 }
-// 페이지 변경해주기!! - 10페이지 뿌려주는 방법 고민해보기
-fetch(
-    "https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=10&serviceKey=uD0LdhujYQQfWxU1YCR6Qyn%2BIesISjKYwPNXK90c0GwfHJxYqrR13xA7T4vM84mXNrPuWMrxKr%2BqEuFXY9v3nQ%3D%3D"
-)
-.then((r) => r.json())
-.then((r) => console.log(r));
+
+
+
 
 
